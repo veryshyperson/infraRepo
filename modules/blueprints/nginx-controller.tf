99cc2b76d91987ea6dd70e2686657d869fe08a64
veryshyperson <ranregev18@gmail.com>
2025-01-29 02:17:23 +0200

    added README.md

--- modules/blueprints/nginx-controller.tf
+++ modules/blueprints/nginx-controller.tf
@@ -9,6 +9,53 @@ resource "helm_release" "nginx-controller" {
     name  = "controller.service.type"
     value = "ClusterIP"
   }
+  set {
+    name  = "controller.ingressClassResource.name"
+    value = "nginx"
+  }
+
+  set {
+    name  = "controller.ingressClassResource.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "controller.metrics.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "controller.metrics.serviceMonitor.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "controller.metrics.serviceMonitor.additionalLabels.release"
+    value = "prometheus"
+  }
+
+  set {
+    name  = "controller.podAnnotations.prometheus\\.io/port"
+    value = "10254"
+  }
+
+  set {
+    name  = "controller.podAnnotations.prometheus\\.io/scrape"
+    value = "true"
+  }
+
+  set {
+    name  = "controller.extraArgs.metrics-per-host"
+    value = "false"
+  }
+
+  set {
+    name  = "externalTrafficPolicy"
+    value = "Cluster"
+  }
+
   timeout = 900
   wait    = true
+
+
 }
